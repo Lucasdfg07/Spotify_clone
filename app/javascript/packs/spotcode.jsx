26532664adf8b9c57543401f4b105a56326d37b6
Lucas Siqueira Fernandes <lucassiqueirafernandes07@gmail.com>
2020-04-15 16:03:01 -0300

    setting react to the project

---
+++ app/javascript/packs/spotcode.jsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+ 
+document.addEventListener('DOMContentLoaded', () => {
+  ReactDOM.render(
+    <div>Hello Spotcode :)</div>,
+    document.getElementById('root'),
+  )
+})
