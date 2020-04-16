5ecb356b50b52cc2ed864bffedf52be650e500dd
Lucas Siqueira Fernandes <lucassiqueirafernandes07@gmail.com>
2020-04-15 22:34:57 -0300

    setting discovery functionality and some views

---
+++ app/javascript/components/album/index.js
@@ -0,0 +1,22 @@
+import React, {Fragment} from 'react';
+import { Heading, Image } from 'react-bulma-components';
+import styled from 'styled-components'
+import { Link } from "react-router-dom";
+
+const DivVSpaced = styled.div`
+  margin-top: 10px;
+`
+
+const Album = (props) => {
+  return(
+    <Link to={`/album/${props.id}`}>
+      <Image src={props.cover_url}/>
+
+      <DivVSpaced>
+        <Heading size={6} className='has-text-white'>{props.title}</Heading>
+        <Heading size={6} className='has-text-white' subtitle>{props.artist_name}</Heading>
+      </DivVSpaced>
+    </Link>
+  );
+}
+export default Album;
--- app/javascript/screens/album/index.js
+++ app/javascript/screens/album/index.js
@@ -1,9 +1,12 @@
   import React, { Fragment, useState } from 'react';
-      
+  import NavbarFooter from '../../components/common/navbar_footer';
+
+
   const AlbumsScreen = () => {
     return(
       <Fragment>
         <div>Albums Screen</div>
+        <NavbarFooter/>
       </Fragment>
     );
   }
